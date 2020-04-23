f74370b329d5083968361865a646233b12cee152
PepperFreeman <yzk4v@mail.umkc.edu>
2020-04-23 13:14:03 -0500

    parent signup consent form checkbox

--- zipCode/client/src/components/signup/FormParent.js
+++ zipCode/client/src/components/signup/FormParent.js
@@ -4,6 +4,39 @@ import { Link } from "react-router-dom";
 import validateEmails from "../../utils/validateEmails";
 import ParentInfo from "./ParentInfo";
 import ParentSchema from "./ParentSchema";
+import Background from './chalkboard.png'
+import { render } from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Checkbox from 'material-ui/Checkbox';
+var sectionStyle = {
+  width: "98%",
+  height: "750px",
+  backgroundImage: `url(${Background})`
+};
+function onCheck(event) {
+  console.log('onCheck: ', event); // always called
+}
+function onLinkClick(event) {
+  console.log('onLinkClick'); // never called
+}
+const label = (
+  <span>I have read and agree to the&nbsp;
+    <a
+      href="/"
+      target="_blank"
+      onClick={onLinkClick}
+    >
+      Terms and Conditions
+    </a>
+  </span>
+)
+
+// render(
+//     <MuiThemeProvider>
+//         <Checkbox label={label} onCheck={onCheck} />
+//     </MuiThemeProvider>,
+//     document.getElementById('app')
+// )
 
 // import * as actions from "../../actions";
 const _ = require("underscore");
@@ -11,22 +44,33 @@ const _ = require("underscore");
 class ParentForm extends Component {
   render() {
     return (
-      <div>
-        <form
-          className="col s12"
-          onSubmit={this.props.handleSubmit(this.props.onParentSubmit)}
-        >
-          <ParentInfo />
-          <div className="container">
-            <Link to="/accounts/new" className="red btn-flat white-text">
-              Cancel
+      <div style={sectionStyle}>
+
+        <div>
+
+          <form
+            className="col s12"
+            onSubmit={this.props.handleSubmit(this.props.onParentSubmit)}
+          >
+
+            <ParentInfo />
+
+            <br />
+            <br />
+            <MuiThemeProvider>
+              <Checkbox label={label} onCheck={onCheck} />
+            </MuiThemeProvider>
+            <div className="container2">
+              <Link to="/accounts/new" className="red btn-flat white-text">
+                Cancel
             </Link>
-            <button type="submit" className="teal btn-flat right white-text">
-              Next
+              <button type="submit" className="teal btn-flat white-text">
+                Next
               <i className="material-icons right">done</i>
-            </button>
-          </div>
-        </form>
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     );
   }
