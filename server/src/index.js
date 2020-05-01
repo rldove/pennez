fdbc916ef8886f5bc134286d3a4e338fc3db0694
Alex Lane <alex@aml.codes>
2020-05-01 14:44:41 -0400

    static

--- server/src/index.js
+++ server/src/index.js
@@ -40,17 +40,23 @@ require("./routes/readingSourceRoutes")(app);
 require("./routes/authAdminRoutes")(app);
 // require("./server/routes/surveyRoutes")(app);
 
-if (process.env.NODE_ENV === "production") {
-	// express will serve up production assets
-	// Like our main.js file, or main.css file
-	app.use(express.static("../client/build"));
-	// Express will serve up the index.html file
-	// if it doesn't recognize the route
-	const path = require("path");
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-	});
-}
+// if (process.env.NODE_ENV === "production") {
+// 	// express will serve up production assets
+// 	// Like our main.js file, or main.css file
+// 	app.use(express.static("../client/build"));
+// 	// Express will serve up the index.html file
+// 	// if it doesn't recognize the route
+// 	const path = require("path");
+// 	app.get("*", (req, res) => {
+// 		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+// 	});
+// }
+
+app.use(express.static(path.join(__dirname, '../client/build')));
+app.get('/', function (req, res) {
+	console.log(req)
+	res.sendFile(path.join(__dirname, '../cllient/build', 'index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
