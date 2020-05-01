426942413035e8fa1e3f6fbdfafd972e13e179cd
Alex Lane <alex@aml.codes>
2020-04-30 23:11:14 -0400

    start abstracting the api into amplify, project becomes frontend

--- server/src/models/Student.js
+++ server/src/models/Student.js
@@ -2,21 +2,21 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const studentSchema = new Schema({
-  firstName: String,
-  lastName: String,
-  schoolDistrictName: String,
-  state: String,
-  zipCode: Number,
-  currentGradeLevel: String,
-  firstName_lastName_teacher: String,
+	firstName: String,
+	lastName: String,
+	schoolDistrictName: String,
+	state: String,
+	zipCode: Number,
+	currentGradeLevel: String,
+	firstName_lastName_teacher: String,
 
-  username: String,
-  password: String,
+	username: String,
+	password: String,
 
-  lexileReadingLevel: { type: String, default: "" },
-  readingGradeLevel: { type: String, default: "" },
-  accountType: { type: String, default: "Student" },
-  _supervisor: { type: Schema.Types.ObjectId, ref: "User" }
+	lexileReadingLevel: { type: String, default: "" },
+	readingGradeLevel: { type: String, default: "" },
+	accountType: { type: String, default: "Student" },
+	_supervisor: { type: Schema.Types.ObjectId, ref: "User" }
 });
 
 mongoose.model("students", studentSchema);
